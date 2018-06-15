bacf330a66f42767a0896f4afcd58252a1cd9f34
Yosei Ito <y-itou@lumber-mill.co.jp>
2018-06-15 13:51:40 +0900

    Add user list page(only for admin).

--- railsapp/app/controllers/pages_controller.rb
+++ railsapp/app/controllers/pages_controller.rb
@@ -4,4 +4,16 @@ class PagesController < ApplicationController
 
   def dashboard
   end
+
+  def users
+    if user_signed_in?
+      if current_user.admin?
+        @users = User.all
+      else
+        render status: 403
+      end
+    else
+      render status: 403
+    end
+  end
 end
--- railsapp/config/routes.rb
+++ railsapp/config/routes.rb
@@ -4,6 +4,7 @@ Rails.application.routes.draw do
   devise_for :users
 
   get 'pages/dashboard'
+  get 'pages/users'
   root to: 'pages#root'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
