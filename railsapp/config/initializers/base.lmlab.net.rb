5dcefe93cf39b1d3196daade206ed76537644c71
Yosei Ito <y-itou@lumber-mill.co.jp>
2018-07-19 20:55:10 +0900

    Start I18n. See #8

--- railsapp/config/initializers/base.lmlab.net.rb
+++ railsapp/config/initializers/base.lmlab.net.rb
@@ -1,2 +1,5 @@
 PRODUCT_IMAGES_DIR = "/var/www/base.lmlab.net/uploads/products"
 TAG_IMAGES_DIR = "/var/www/base.lmlab.net/uploads/tags"
+
+I18n.available_locales = [:en, :ja]
+I18n.default_locale = :ja # I18n.locale = :ja  does not work here
