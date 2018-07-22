3841193814e029c9924a3e9377e75d7cdd71221a
Yosei Ito <y-itou@lumber-mill.co.jp>
2018-07-22 21:58:10 +0900

    Change default time_zone(UTC -> JST).

--- railsapp/config/initializers/base.lmlab.net.rb
+++ railsapp/config/initializers/base.lmlab.net.rb
@@ -2,4 +2,6 @@ PRODUCT_IMAGES_DIR = "/var/www/base.lmlab.net/uploads/products"
 TAG_IMAGES_DIR = "/var/www/base.lmlab.net/uploads/tags"
 
 I18n.available_locales = [:en, :ja]
-I18n.default_locale = :ja # I18n.locale = :ja  does not work here
+I18n.default_locale = :ja
+# I18n.locale = :ja  does not work here
+# Rails.application.config.time_zone = 'Tokyo'  does not work here. See application.rb
