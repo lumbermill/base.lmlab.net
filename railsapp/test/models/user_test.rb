a1ecc97cecbd226c1befdd4893762ab8a909ec45
Yosei Ito <y-itou@lumber-mill.co.jp>
2018-08-09 20:39:36 +0900

    Added some test cases and fixtures for them. Execute `rake test test/../..rb` to run each test.

--- railsapp/test/models/user_test.rb
+++ railsapp/test/models/user_test.rb
@@ -1,7 +1,13 @@
 require 'test_helper'
 
 class UserTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  test "number of children" do
+    assert_equal 8, User.count
+    # FIXME: The number of admin user's chindren should be 2. But it returns 1..
+    assert_equal 2, User.find(1).children.count
+    assert_equal 3, User.find(2).children.count
+    assert_equal 0, User.find(3).children.count
+    assert_equal 1, User.find(4).children.count
+  end
+
 end
