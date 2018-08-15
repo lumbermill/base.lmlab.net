395ef91fc3346dda927fe6ebc021ff709f7526ab
akieno <forcedec21@gmail.com>
2018-08-15 16:22:18 +0900

    Add some Japanese resources #8

--- railsapp/app/controllers/tags_controller.rb
+++ railsapp/app/controllers/tags_controller.rb
@@ -29,7 +29,7 @@ class TagsController < ApplicationController
 
     respond_to do |format|
       if @tag.save
-        format.html { redirect_to @tag, notice: 'Tag was successfully created.' }
+        format.html { redirect_to @tag, notice: t('Tag') + t('was successfully created') }
         format.json { render :show, status: :created, location: @tag }
       else
         format.html { render :new }
@@ -43,7 +43,7 @@ class TagsController < ApplicationController
   def update
     respond_to do |format|
       if @tag.update(tag_params)
-        format.html { redirect_to @tag, notice: 'Tag was successfully updated.' }
+        format.html { redirect_to @tag, notice: t('Tag') + t('was successfully updated') }
         format.json { render :show, status: :ok, location: @tag }
       else
         format.html { render :edit }
@@ -57,7 +57,7 @@ class TagsController < ApplicationController
   def destroy
     @tag.destroy
     respond_to do |format|
-      format.html { redirect_to tags_url, notice: 'Tag was successfully destroyed.' }
+      format.html { redirect_to tags_url, notice: t('Tag') + t('was successfully destroyed') }
       format.json { head :no_content }
     end
   end
