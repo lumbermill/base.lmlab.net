73679ac95f1b2c443d293a45b1f7699cf6ef3acb
akieno <forcedec21@gmail.com>
2018-08-29 17:04:44 +0900

    Added some fixtures for order model.(in progress)

--- railsapp/test/models/order_test.rb
+++ railsapp/test/models/order_test.rb
@@ -3,11 +3,11 @@ require 'test_helper'
 class OrderTest < ActiveSupport::TestCase
 
   test "count " do
-    assert_equal 3, Order.count
-    assert_equal 0, Order.in_cart.count
+    assert_equal 7, Order.count
+    assert_equal 1, Order.in_cart.count
     assert_equal 3, Order.ordered.count
-    assert_equal 0, Order.shipping.count
-    assert_equal 0, Order.arrived.count
-    assert_equal 0, Order.canceled.count
+    assert_equal 1, Order.shipping.count
+    assert_equal 1, Order.arrived.count
+    assert_equal 1, Order.canceled.count
   end
 end
