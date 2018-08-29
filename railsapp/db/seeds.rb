6dfeffe55fe9afc4000179c514a47532c73bce80
Yosei Ito <y-itou@lumber-mill.co.jp>
2018-08-29 13:15:45 +0900

    Added some fixtures for order model.

--- railsapp/db/seeds.rb
+++ railsapp/db/seeds.rb
@@ -22,15 +22,25 @@ if Rails.env == "development"
   tag1 = Tag.create(code: "supplement", name: "サプリメント")
   tag2 = Tag.create(code: "food", name: "食品")
 
-  Product.create(code: "5050", name: "トリプルＸ（レフィル）", copy: "究極の活性酸素対策！",
+  p1 = Product.create(code: "5050", name: "トリプルＸ（レフィル）", copy: "究極の活性酸素対策！",
     price: 10000, user: admin, tags: [tag1], keywords: "")
-  Product.create(code: "0029", name: "ハーブラスト", copy: "究極ののど飴！", price: 120, user: admin, tags: [tag2])
-  Product.create(code: "0581", name: "ハーブラスト", size: "１０本入り", price: 1180, user: admin, tags: [tag2])
-  Product.create(code: "5070", name: "プロテイン シェーカー", copy: "", user: admin)
-  Product.create(code: "5165", name: "ニュートリ プロテイン （オールプラント）", user: admin)
+  p2 = Product.create(code: "0029", name: "ハーブラスト", copy: "究極ののど飴！", price: 120, user: admin, tags: [tag2])
+  p3 = Product.create(code: "0581", name: "ハーブラスト", size: "１０本入り", price: 1180, user: admin, tags: [tag2])
+  p4 = Product.create(code: "5070", name: "プロテイン シェーカー", copy: "", user: admin)
+  p5 = Product.create(code: "5165", name: "ニュートリ プロテイン （オールプラント）", user: admin)
   Product.create(code: "5910", name: "ニュートリ プロテイン （オールプラント）", size: "お徳用", user: admin)
   Product.create(code: "5441", name: "ＸＳ エナジードリンク エクストラバーン リモンチェッロ パイレーツブラスト", size: "６本入り", user: admin)
   Product.create(code: "5414", name: "ＸＳ エナジードリンク エクストラバーン ミックスベリーブラスト", size: "６本入り", user: admin)
   FileUtils.cp Dir.glob("db/seeds/products/*.*"), PRODUCT_IMAGES_DIR
   FileUtils.cp Dir.glob("db/seeds/tags/*.*"), TAG_IMAGES_DIR
+
+  t3 = Time.now
+  t2 = t3 - 1.day
+  t1 = t3 - 3.day
+  Order.create(user: u1, checkout_at: t3,product_id: p1.id, quantity: 1, price: p1.price*1, status: "ordered")
+  Order.create(user: u1, checkout_at: t2,product_id: p2.id, quantity: 2, price: p2.price*2, status: "ordered")
+  Order.create(user: u2, checkout_at: t2,product_id: p1.id, quantity: 3, price: p3.price*3, status: "ordered")
+  Order.create(user: u1, checkout_at: t1,product_id: p3.id, quantity: 1, price: p3.price*1, status: "shipping")
+  Order.create(user: u1, checkout_at: t1,product_id: p1.id, quantity: 1, price: p1.price*1, status: "arrived")
+  Order.create(user: u2, checkout_at: t1,product_id: p4.id, quantity: 2, price: p4.price*2, status: "canceled")
 end
--- railsapp/test/models/order_test.rb
+++ railsapp/test/models/order_test.rb
@@ -3,7 +3,11 @@ require 'test_helper'
 class OrderTest < ActiveSupport::TestCase
 
   test "count " do
-    assert_equal 2, Order.count
+    assert_equal 3, Order.count
+    assert_equal 0, Order.in_cart.count
+    assert_equal 3, Order.ordered.count
+    assert_equal 0, Order.shipping.count
+    assert_equal 0, Order.arrived.count
+    assert_equal 0, Order.canceled.count
   end
-
 end
