468c53d825a8d84ee25aee5af80ade47f7c92b3e
Yosei Ito <y-itou@lumber-mill.co.jp>
2018-09-23 10:24:24 +0900

    Addes links for suppliers, etc.

--- railsapp/app/helpers/products_helper.rb
+++ railsapp/app/helpers/products_helper.rb
@@ -1,2 +1,23 @@
 module ProductsHelper
+  def makers
+    MAKERS.keys.join(", ")
+  end
+
+  def makers_url(product)
+    html = ""
+    MAKERS.keys.each do |m|
+      maker = MAKERS[m]
+      if product.code
+        url = maker[:url4product].gsub("{code}",product.code.to_s)
+      else
+        url = maker[:url]
+      end
+      name = maker[:name]
+      if product.maker == m.to_s
+        # FIXME: i18n
+        html += "<a href='#{url}' target='#{m}'>#{name} で検索</a> "
+      end
+    end
+    return html
+  end
 end
--- railsapp/config/initializers/base.lmlab.net.rb
+++ railsapp/config/initializers/base.lmlab.net.rb
@@ -5,3 +5,14 @@ I18n.available_locales = [:en, :ja]
 I18n.default_locale = :ja
 # I18n.locale = :ja  does not work here
 # Rails.application.config.time_zone = 'Tokyo'  does not work here. See application.rb
+
+# They are like suppliers.
+makers = {
+  amway: {name: "Amway", url: "https://amwaylive.com",
+    url4product: "https://www1.amwaylive.com/search/Search?keyword={code}"
+  },
+  ksy: {name: "KSY", url: "https://raspberry-pi.ksyic.com/",
+    url4product: "https://raspberry-pi.ksyic.com/main/index/pdp.id/{code}/pdp.open/{code}"
+  }}
+
+MAKERS = makers
