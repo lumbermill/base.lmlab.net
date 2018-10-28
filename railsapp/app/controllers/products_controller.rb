00a8257b062f42bc81e880f37bce47415cfe2db3
Yosei Ito <y-itou@lumber-mill.co.jp>
2018-10-28 22:51:16 +0900

    Fixed problem of creating new product.

--- railsapp/app/controllers/products_controller.rb
+++ railsapp/app/controllers/products_controller.rb
@@ -119,7 +119,6 @@ class ProductsController < ApplicationController
     # Use callbacks to share common setup or constraints between actions.
     def set_product
       @product = Product.find(params[:id])
-      @tags = Tag.order("code")
     end
 
     # Never trust parameters from the scary internet, only allow the white list through.
