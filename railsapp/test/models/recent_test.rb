0d2002a401c6ae210b884cab7b3a944fb4830dab
Yosei Ito <y-itou@lumber-mill.co.jp>
2018-10-31 22:55:53 +0900

    Added an assertion in the test case for Recent model.

--- railsapp/test/models/recent_test.rb
+++ railsapp/test/models/recent_test.rb
@@ -7,12 +7,12 @@ class RecentTest < ActiveSupport::TestCase
     p1 = Product.find(1)
     p2 = Product.find(2)
 
+    assert_equal 2, Recent.count
     Recent.append(u1,p1)
     Recent.append(u2,p1)
     Recent.append(u2,p2)
     Recent.append(u2,p2)
     Recent.append(u2,p1)
-
     assert_equal 5, Recent.count
   end
 end
