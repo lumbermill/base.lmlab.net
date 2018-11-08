bd73ca508194996067a749ed8dbd0b2efdf8a45f
Yosei Ito <y-itou@lumber-mill.co.jp>
2018-11-08 20:56:40 +0900

    Added helper methods for Slack notification. (Webhook URL is for test)

--- railsapp/app/helpers/application_helper.rb
+++ railsapp/app/helpers/application_helper.rb
@@ -1,2 +1,14 @@
 module ApplicationHelper
+  def self.slack_init
+    # return if Rails.env.test?
+    url = 'https://hooks.slack.com/services/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'
+    notifier = Slack::Notifier.new(url)
+    yield(notifier)
+  end
+
+  def self.slack_ping(msg="Hello, world!")
+    slack_init do |notifier|
+      notifier.ping(msg)
+    end
+  end
 end
