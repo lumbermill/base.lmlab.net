85b01aeec34cd090c076258fd91cc674da3f971f
Yosei Ito <y-itou@lumber-mill.co.jp>
2020-03-05 19:57:04 +0900

    update capisstrano.

--- railsapp/config/deploy.rb
+++ railsapp/config/deploy.rb
@@ -1,5 +1,5 @@
 # config valid for current version and patch releases of Capistrano
-lock "~> 3.11.0"
+lock "~> 3.12.0"
 
 set :application, "base"
 set :repo_url, "git@github.com:lumbermill/base.lmlab.net.git"
